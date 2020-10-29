70bce99642ec4cc19345ac80e61aa86f28859401
xMate420x <ipcimtolvaj@gmail.com>
2020-10-29 16:26:28 +0000

    Update rng.py

--- pseudocode/rng.py
+++ pseudocode/rng.py
@@ -1,9 +1,9 @@
 import random;
 
 def rng(sentencenum):
-    prefixes = ["refactored","reimagined","stunning","sturdy","solid","cuddly","literate","animated","silver","psychic","congenital","crispy","special","legendary","expert"]
-    suffixes = ["pancake","guide","succotash","lamp","happiness","machine","carnival","umbrella","system","waddle","potato","tree","parakeet","guacamole","eureka","fortnight"]
-    infixes = ["octo", "computing", "palm"]
+    prefixes = ["refactored","reimagined","stunning","sturdy","solid","cuddly","literate","animated","silver","psychic","congenital","crispy","special","legendary","expert","friendly","shiny","curly","bug-free","didactic","cautious","laughing","redesigned","fictional","improved","glowing","bookish","probable","upgraded"]
+    suffixes = ["pancake","guide","succotash","lamp","happiness","computing-machine","carnival","umbrella","system","waddle","potato","palm-tree","parakeet","guacamole","eureka","fortnight","chainsaw","garbanzo","winner","goggles","memory","carnival","pancake","robot","doodle","funicular","giggle","fiesta","disco","waffle","bassoon","couscous","broccoli","succotash"]
+    infixes = ["octo"]
 
     if(sentencenum == 3): print(random.choice(prefixes)+"-"+random.choice(infixes)+"-"+random.choice(suffixes))
     else: print(random.choice(prefixes)+"-"+random.choice(suffixes))
